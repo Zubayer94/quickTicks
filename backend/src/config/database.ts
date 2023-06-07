842cf8aa1c3a8a0e8670a56362e19ae2d0ff2d55
Zubayer Hossain <hossain@tikweb.com>
2023-06-07 19:49:28 +0600

    feat: updated database connection

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,21 +1,27 @@
-import { Dialect, Model, Sequelize } from 'sequelize'
-import { logger } from './logger'
-import { DB_HOST, DB_LOGGING, DB_NAME, DB_PASS, DB_USER } from './envs'
+import { Sequelize } from "sequelize";
+import { logger } from "./logger";
+import { DB_HOST, DB_LOGGING, DB_NAME, DB_PASS, DB_USER } from "./envs";
+
+const db = new Sequelize({
+  host: DB_HOST,
+  database: DB_NAME,
+  username: DB_USER,
+  password: DB_PASS,
+  dialect: "mysql",
+  logging: DB_LOGGING ? (msg: any) => logger.debug(msg) : false,
+});
 
 
-const sequelize = new Sequelize({
-    host: DB_HOST,
-    database: DB_NAME,
-    username: DB_USER,
-    password: DB_PASS,
-    dialect: 'mysql',
-    logging: DB_LOGGING ? (msg: any) => logger.debug(msg) : false,
-})
+db
+  .authenticate()
+  .then(() => {
+    logger.info("Connection has been established successfully.");
+  })
+  .catch((error: any) => {
+    logger.error("Unable to connect to the database: ", error);
+  });
 
-sequelize.authenticate().then(() => {
-    logger.info('Connection has been established successfully.');
- }).catch((error: any) => {
-    logger.error('Unable to connect to the database: ', error);
- });
+export default db
+
 
 // logging: msg => logger.debug(msg),     // Use custom logger (e.g. Winston or Bunyan), displays the first parameter
