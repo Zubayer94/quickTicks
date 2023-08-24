676ecf86b92bf4db280ab22dd6fc92c8a9dc52fb
Zubayer Hossain <hossain@tikweb.com>
2023-08-24 16:55:37 +0600

    added errorMiddleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import db from "./config/database";
 import { DB_FORCE, PORT } from "./config/envs";
 import { createServer } from "./config/express";
 import { logger } from "./config/logger";
+import errorMiddleware from "./api/middlewares/error";
 
 (async function startServer() {
   const app = createServer();
 
   await db.sync({ force: DB_FORCE == 'true' ? true : false})
     .then(() => { logger.info("Database connected"); })
+
+  app.use(errorMiddleware);
 
   app.listen(PORT, () => {
     logger.info(`Listening on http://localhost:${PORT}`);
