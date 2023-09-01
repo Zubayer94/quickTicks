2a4a0c61c7e1da38c20e212200f37be9eee34d8d
Zubayer Hossain <hossain@tikweb.com>
2023-09-01 20:02:19 +0600

    updated IWrite

--- backend/src/api/interfaces/IWrite.ts
+++ backend/src/api/interfaces/IWrite.ts
@@ -1,5 +1,7 @@
+import { Transaction } from 'sequelize'
+
 export interface IWrite<T> {
-  create(item: T): Promise<boolean>;
-  update(id: string, item: T): Promise<boolean>;
+  create(item: T, transaction: Transaction): Promise<boolean>;
+  update(id: string, item: T, transaction: Transaction): Promise<boolean>;
   delete(id: string): Promise<boolean>;
 }
