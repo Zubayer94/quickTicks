b261da9dd6d3abefa7d051caaf3f3a3e531045a0
Zubayer Hossain <hossain@tikweb.com>
2023-09-01 20:02:45 +0600

    updated BaseRepository

--- backend/src/api/repositories/base/BaseRepository.ts
+++ backend/src/api/repositories/base/BaseRepository.ts
@@ -1,41 +1,45 @@
 // import all interfaces
 import { IWrite } from '../../interfaces/IWrite';
 import { IRead } from '../../interfaces/IRead';
-import { Model } from 'sequelize';
-import db from '../../../config/database'
+import { Mutation, Setter } from '../../types/crudOperations';
 
 // that class only can be extended
+// export abstract class BaseRepository<T extends Model<InferAttributes<T>, InferCreationAttributes<T>>> 
 export abstract class BaseRepository<T> implements IWrite<T>, IRead<T> {
   //creating a property to use your code in all instances 
   // that extends your base repository and reuse on methods of class
-  public readonly _collection: typeof Model;
+  // public readonly _collection: T;
 
   //we created constructor with arguments to manipulate mongodb operations
-  constructor(modelName: string) {
-    this._collection = db._[modelName] as any;
+  constructor(protected readonly _model: any) {
   }
-
-  // we add to method, the async keyword to manipulate the insertOne result
-  // of method.
-  async create(item: T): Promise<boolean> {
-    const result = await this._collection.create(
+  
+  create: Setter<T, boolean> = async (item, t) => {
+    const result = await this._model.create(
 			item
 		);
-    // after the insert operations, we returns only acknowledged property (that haves a 1 or 0 results)
-    // and we convert to boolean result (0 false, 1 true)
-    return !!result.acknowledged;
+
+    return !!result;
   }
 
+  update: Mutation<string, T, boolean> = async (id, item, t) => {
+    const result = await this._model.update(item, {
+      where: {
+        id: id,
+      },
+    })
 
-  update(id: string, item: T): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    return !!result;
   }
+
   delete(id: string): Promise<boolean> {
     throw new Error('Method not implemented.');
   }
+
   find(item: T): Promise<T[]> {
     throw new Error('Method not implemented.');
   }
+
   findOne(id: string): Promise<T> {
     throw new Error('Method not implemented.');
   }
